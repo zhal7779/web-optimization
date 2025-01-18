72b1b0435ace45ee76722a2c0c74f87ce316c938
minyoung <zhal7779@naver.com>
2025-01-19 01:27:04 +0900

    Refactor: 폰트 최적화 - 폰트 적용 시점 컨트롤

--- lecture-3/src/components/BannerVideo.js
+++ lecture-3/src/components/BannerVideo.js
@@ -1,23 +1,42 @@
-import React from 'react'
-import video from '../assets/banner-video.mp4'
+import React, { useState, useEffect } from "react";
+import video from "../assets/banner-video.mp4";
+import FontFaceObserver from "fontfaceobserver";
 
 function BannerVideo() {
-	return (
-		<div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
-			<div className="absolute h-screen w-full left-1/2">
-				<video src={video} className="absolute translateX--1/2 h-screen max-w-none min-w-screen -z-1 bg-black min-w-full min-h-screen" autoPlay loop muted/>
-			</div>
-			<div className="w-full h-full flex justify-center items-center">
-				<div className="text-white text-center">
-					<div className="text-6xl leading-none font-semibold">KEEP</div>
-					<div className="text-6xl leading-none font-semibold">CALM</div>
-					<div className="text-3xl leading-loose">AND</div>
-					<div className="text-6xl leading-none font-semibold">RIDE</div>
-					<div className="text-5xl leading-tight font-semibold">LONGBOARD</div>
-				</div>
-			</div>
-		</div>
-	)
+  const [isFontLoaded, setIsFontLoaded] = useState(false);
+  const font = new FontFaceObserver("BMYEONSUNG");
+
+  useEffect(() => {
+    font.load().then(function () {
+      setIsFontLoaded(true);
+    });
+  }, []);
+
+  return (
+    <div
+      className="BannerVideo w-full h-screen overflow-hidden relative bg-texture"
+      style={{ opacity: isFontLoaded ? 1 : 0, transition: "opacity 0.3s ease" }}
+    >
+      <div className="absolute h-screen w-full left-1/2">
+        <video
+          src={video}
+          className="absolute translateX--1/2 h-screen max-w-none min-w-screen -z-1 bg-black min-w-full min-h-screen"
+          autoPlay
+          loop
+          muted
+        />
+      </div>
+      <div className="w-full h-full flex justify-center items-center">
+        <div className="text-white text-center">
+          <div className="text-6xl leading-none font-semibold">KEEP</div>
+          <div className="text-6xl leading-none font-semibold">CALM</div>
+          <div className="text-3xl leading-loose">AND</div>
+          <div className="text-6xl leading-none font-semibold">RIDE</div>
+          <div className="text-5xl leading-tight font-semibold">LONGBOARD</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default BannerVideo
+export default BannerVideo;
